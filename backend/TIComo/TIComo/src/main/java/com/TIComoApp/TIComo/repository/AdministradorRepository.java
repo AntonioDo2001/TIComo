60e5fac966ff6bbcbce78403cfa4120f8b6c9ec7
Roberto Ortuño Blanco <91477159+robertoOB07@users.noreply.github.com>
2022-11-01 20:08:47 +0100

    Delete backend directory

--- backend/TIComo/TIComo/src/main/java/com/TIComoApp/TIComo/repository/AdministradorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.TIComoApp.TIComo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.TIComoApp.TIComo.model.Administrador;
-
-public interface AdministradorRepository extends MongoRepository<Administrador,String>{
-
-}
