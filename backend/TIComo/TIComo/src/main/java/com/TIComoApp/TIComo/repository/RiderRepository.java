816ef4f56fe5d2f9c6b0cd65e897d11d754759cd
Roberto Ortuño Blanco <91477159+robertoOB07@users.noreply.github.com>
2022-11-01 20:10:01 +0100

    Delete backend directory

--- backend/TIComo/TIComo/src/main/java/com/TIComoApp/TIComo/repository/RiderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.TIComoApp.TIComo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.TIComoApp.TIComo.model.Rider;
-
-public interface RiderRepository extends MongoRepository<Rider,String>{
-
-}
