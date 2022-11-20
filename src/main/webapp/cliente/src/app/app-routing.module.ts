718fc8914c717f3431958503eda8fb61cb55f75a
Roberto Ortuño Blanco <91477159+robertoOB07@users.noreply.github.com>
2022-11-21 00:15:08 +0100

    Add files via upload

--- src/main/webapp/cliente/src/app/app-routing.module.ts
+++ src/main/webapp/cliente/src/app/app-routing.module.ts
@@ -19,23 +19,56 @@ import { ListarPlatosClienteComponent } from './components/listar-platos-cliente
 import { ListarRestaurantesClienteComponent } from './components/listar-restaurantes-cliente/listar-restaurantes-cliente.component';
 import { RegisterComponent } from './components/register/register.component';
 import { EditarPerfilClienteComponent } from './components/editar-perfil-cliente/editar-perfil-cliente.component';
+import { PantallaCargaInicialComponent } from './components/pantalla-carga-inicial/pantalla-carga-inicial.component';
+import { CrearPedidoComponent } from './components/crear-pedido/crear-pedido.component';
+import { ListarPedidosRealizadosClienteComponent } from './components/listar-pedidos-realizados-cliente/listar-pedidos-realizados-cliente.component';
+import { ListarPedidosClienteComponent } from './components/listar-pedidos-cliente/listar-pedidos-cliente.component';
+import { ListarEntregasClienteComponent } from './components/listar-entregas-cliente/listar-entregas-cliente.component';
+import { PaginaPrincipalRiderComponent } from './components/pagina-principal-rider/pagina-principal-rider.component';
+import { ListarEntregasRiderComponent } from './components/listar-entregas-rider/listar-entregas-rider.component';
+import { RealizarValoracionComponent } from './components/realizar-valoracion/realizar-valoracion.component';
+import { EntregasClientesComponent } from './components/entregas-clientes/entregas-clientes.component';
+import { VerValoracionesRestauranteComponent } from './components/ver-valoraciones-restaurante/ver-valoraciones-restaurante.component';
+import { VerValoracionesRestauranteAdminComponent } from './components/ver-valoraciones-restaurante-admin/ver-valoraciones-restaurante-admin.component';
+import { VerValoracionesRiderComponent } from './components/ver-valoraciones-rider/ver-valoraciones-rider.component';
+import { VerFacturaComponent } from './components/ver-factura/ver-factura.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
+  { path: '', component: PantallaCargaInicialComponent},
+  { path: 'pantallaInicial', component: PantallaCargaInicialComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'pagina-principal', component: PaginaPrincipalComponent},
   { path: 'pagina-principal-cliente', component: PaginaPrincipalClienteComponent},
+  { path: 'pagina-principal-rider', component: PaginaPrincipalRiderComponent},
   { path: 'listar-riders', component: ListarRidersComponent},
   { path: 'editarPerfil-cliente', component: EditarPerfilClienteComponent},
   { path: 'listar-platos-cliente', component: ListarPlatosClienteComponent},
   { path: 'listar-restaurantes-cliente', component: ListarRestaurantesClienteComponent},
+  { path: 'ver-valoraciones-restaurante/:nombre', component: VerValoracionesRestauranteComponent},
+  { path: 'ver-valoraciones-restaurante', component: VerValoracionesRestauranteComponent},
+  { path: 'ver-valoraciones-restaurante-admin/:nombre', component: VerValoracionesRestauranteAdminComponent},
+  { path: 'ver-valoraciones-restaurante-admin', component: VerValoracionesRestauranteAdminComponent},
+  { path: 'ver-valoraciones-rider/:id', component: VerValoracionesRiderComponent},
+  { path: 'ver-valoraciones-rider', component: VerValoracionesRiderComponent},
+  { path: 'ver-factura/:id', component: VerFacturaComponent},
+  { path: 'ver-factura', component: VerFacturaComponent},
   { path: 'crear-rider', component: CrearRiderComponent},
   { path: 'editar-rider/:id', component: CrearRiderComponent},
   { path: 'editar-plato/:id', component: CrearPlatoComponent},
   { path: 'listar-platos', component: ListarPlatosComponent},
+  { path: 'listar-pedidos', component: ListarPedidosClienteComponent},
+  { path: 'listar-pedidos-realizados', component: ListarPedidosRealizadosClienteComponent},
+  { path: 'listar-entregas-cliente', component: ListarEntregasClienteComponent},
+  { path: 'estregas-cliente', component: EntregasClientesComponent},
+  { path: 'listar-entregas-rider', component: ListarEntregasRiderComponent},
+  { path: 'listar-pedidos-realizados-cliente', component: ListarPedidosRealizadosClienteComponent},
   { path: 'crear-plato', component: CrearPlatoComponent},
   { path: 'crear-plato/:id', component: CrearPlatoComponent},
+  { path: 'crear-pedido', component: CrearPedidoComponent},
+  { path: 'crear-pedido/:id', component: CrearPedidoComponent},
+  { path: 'realizar-valoracion', component: RealizarValoracionComponent},
+  { path: 'realizar-valoracion/:id', component: RealizarValoracionComponent},
   { path: 'listar-admins', component: ListarAdminsComponent},
   { path: 'crear-admin', component: CrearAdminComponent},
   { path: 'editar-admin/:id', component: CrearAdminComponent},
